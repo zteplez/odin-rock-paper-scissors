7253b4c1469f280d94b703e48706b9b34fc1cf76
zteplez <asibrahim98@gmail.com>
2024-09-25 11:12:22 +0300

    [feat]:Add playRound function.
    This function takes two parameters, human choice and computer choice.
    Compares their values and increases scores, in the end logs final state.

--- app.js
+++ app.js
@@ -1,26 +1,69 @@
-let humanScore, computerScore = 0;
+let humanScore = 0, computerScore = 0;
+
+function playRound(hChoice, cChoice) {
+    hChoice = hChoice.toLowerCase();
+    console.log("They are choices " + hChoice + "  " + cChoice)
+
+    if (cChoice == hChoice) { console.log("draw!"); return }
+
+    if (cChoice == "rock") {
+        if (hChoice == "paper") {
+            console.log("you won!");
+            humanScore++;
+        }
+        else {
+            console.log("you lose!");
+            computerScore++;
+        }
+    }
+    else if (cChoice == "paper") {
+        if (hChoice == "rock") {
+            console.log("you lose!");
+            computerScore++;
+        }
+        else {
+            console.log("you won!")
+            humanScore++;
+        }
+    }
+    else if (cChoice == "scissors") {
+        if (hChoice == "paper") {
+            console.log("you lose!");
+            computerScore++;
+        }
+        else {
+            console.log("you won!");
+            humanScore++;
+        }
+    }
+    console.log(humanScore, computerScore);
+}
 
 function getComputerChoice() {
     let randomChoice = Math.floor(Math.random() * 3);
 
     switch (randomChoice) {
         case 0:
-            return "Rock";
+            return "rock";
             break;
         case 1:
-            return "Paper";
+            return "paper";
         case 2:
-            return "Scissors";
+            return "scissors";
             break;
         default:
             console.log("test");
             break;
     }
 }
-function getHumanChoice(){
+function getHumanChoice() {
     let userInput = prompt("0-Rock, 1-Paper, 2-Scissors");
     return userInput;
 }
 
+let human = getHumanChoice();
+let comp = getComputerChoice();
+
+playRound(human, comp);
 
 

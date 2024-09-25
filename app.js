a521b07d4a6a0fe5d9c780c063a62f78a13d7328
zteplez <asibrahim98@gmail.com>
2024-09-25 10:52:04 +0300

    [feat]:Add getComputerChoice function

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("Test log");
+function getComputerChoice() {
+    let randomChoice = Math.floor(Math.random() * 3);
+
+    switch (randomChoice) {
+        case 0:
+            return "Rock";
+            break;
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
+            break;
+        default:
+            console.log("test");
+            break;
+    }
+}
+console.log(getComputerChoice());
